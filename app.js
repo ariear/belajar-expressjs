8510bce961088c51a439d115daacb2501a3610c2
arie <arieinjector@gmail.com>
2022-03-20 22:42:30 +0700

    env

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.get('/about', (request, response) => {
 })
 
 
-const port = 3000
+const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
